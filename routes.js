12637a6337f9f23d61614f6dfe26ce99d53700c6
Junyong Park <jyp0802@hotmail.com>
2017-12-03 20:51:36 +0900

    a

--- routes.js
+++ routes.js
@@ -45,82 +45,157 @@ module.exports = function(app, passport) {
 	});
 
 	app.get('/details', isLoggedIn, function(req, res) {
-		connection.query("SELECT * FROM Classes where cid = ?", [req.query.cid], function(err1, rows1) {
-			if (err1)
-				console.log(err1);
-			if (rows1.length == 0)
-				res.redirect('/confirm?t=de');
-			else {
-				connection.query("SELECT * FROM Users where uid = ? and cid = ?", [req.user.uid, req.query.cid], function(err2, rows2) {
-					if (err2)
-						console.log(err2);
-					res.render('detail.ejs', {classinfo : rows1[0], registered : (rows2.length > 0)});
-				});
-			}
-		});
+		if (req.query.t == "c") {
+			connection.query("SELECT * FROM Classes where cid = ?", [req.query.cid], function(err1, rows1) {
+				if (err1)
+					console.log(err1);
+				if (rows1.length == 0)
+					res.redirect('/confirm?t=de');
+				else {
+					connection.query("SELECT * FROM Users where uid = ? and cid = ?", [req.user.uid, req.query.cid], function(err2, rows2) {
+						if (err2)
+							console.log(err2);
+						res.render('detail.ejs', {type : "class", classinfo : rows1[0], registered : (rows2.length > 0)});
+					});
+				}
+			});
+		}
+		else {
+			connection.query("SELECT * FROM Bible where bid = ?", [req.query.bid], function(err1, rows1) {
+				if (err1)
+					console.log(err1);
+				if (rows1.length == 0)
+					res.redirect('/confirm?t=de');
+				else {
+					connection.query("SELECT * FROM Users where uid = ? and bid = ?", [req.user.uid, req.query.bid], function(err2, rows2) {
+						if (err2)
+							console.log(err2);
+						res.render('detail.ejs', {type : "bible", bibleinfo : rows1[0], registered : (rows2.length > 0)});
+					});
+				}
+			});
+		}
 	});
 
 	app.get('/cancel', isLoggedIn, function(req, res) {
-		connection.query("SELECT cid from Users where uid = ?", [req.user.uid], function(err, rows) {
-			if (err)
-				res.redirect('/confirm?t=e');
-			else if (rows[0].cid == null)
-				res.redirect('/confirm?t=cet');
-			else {
-				connection.query("UPDATE Classes SET current = (current - 1) where cid = ?", [req.query.cid], function(err1, rows1) {
-					if (err1)
-						res.redirect('/confirm?t=ce');
-					else {
-						connection.query("UPDATE Users SET cid = null where uid = ?", [req.user.uid], function(err, rows) {
-							if (err)
-								res.redirect('/confirm?t=e');
-							else
-								res.redirect('/confirm?t=cs');
-						})
-					}
-				})
-			}
-		})
+		if (req.query.t == "c") {
+			connection.query("SELECT cid from Users where uid = ?", [req.user.uid], function(err, rows) {
+				if (err)
+					res.redirect('/confirm?t=e');
+				else if (rows[0].cid == null)
+					res.redirect('/confirm?t=cet');
+				else {
+					connection.query("UPDATE Classes SET current = (current - 1) where cid = ?", [req.query.cid], function(err1, rows1) {
+						if (err1)
+							res.redirect('/confirm?t=ce');
+						else {
+							connection.query("UPDATE Users SET cid = null where uid = ?", [req.user.uid], function(err, rows) {
+								if (err)
+									res.redirect('/confirm?t=e');
+								else
+									res.redirect('/confirm?t=cs');
+							})
+						}
+					})
+				}
+			})
+		}
+		else {
+			connection.query("SELECT bid from Users where uid = ?", [req.user.uid], function(err, rows) {
+				if (err)
+					res.redirect('/confirm?t=e');
+				else if (rows[0].bid == null)
+					res.redirect('/confirm?t=cet');
+				else {
+					connection.query("UPDATE Bible SET current = (current - 1) where bid = ?", [req.query.bid], function(err1, rows1) {
+						if (err1)
+							res.redirect('/confirm?t=ce');
+						else {
+							connection.query("UPDATE Users SET bid = null where uid = ?", [req.user.uid], function(err, rows) {
+								if (err)
+									res.redirect('/confirm?t=e');
+								else
+									res.redirect('/confirm?t=cs');
+							})
+						}
+					})
+				}
+			})
+		}
 	})
 
 	app.get('/register', isLoggedIn, function(req, res) {
-		connection.query("SELECT cid from Users where uid = ?", [req.user.uid], function(err, rows) {
-			if (err)
-				res.redirect('/confirm?t=e');
-			else if (rows[0].cid == req.query.cid)
-				res.redirect('/confirm?t=ret');
-			else if (rows[0].cid != null)
-				res.redirect('/confirm?t=red');
-			else {
-				connection.query("UPDATE Classes SET current = (current + 1) where cid = ?", [req.query.cid], function(err1, rows1) {
-					if (err1)
-						res.redirect('/confirm?t=ref');
-					else {
-						connection.query("UPDATE Users SET cid = ? where uid = ?", [req.query.cid, req.user.uid], function(err, rows) {
-							if (err)
-								res.redirect('/confirm?t=e');
-							else
-								res.redirect('/confirm?t=rs');
-						})
-					}
-				})
-			}
-		})
+		if (req.query.t == "c") {
+			connection.query("SELECT cid from Users where uid = ?", [req.user.uid], function(err, rows) {
+				if (err)
+					res.redirect('/confirm?t=e');
+				else if (rows[0].cid == req.query.cid)
+					res.redirect('/confirm?t=ret');
+				else if (rows[0].cid != null)
+					res.redirect('/confirm?t=red');
+				else {
+					connection.query("UPDATE Classes SET current = (current + 1) where cid = ?", [req.query.cid], function(err1, rows1) {
+						if (err1)
+							res.redirect('/confirm?t=ref');
+						else {
+							connection.query("UPDATE Users SET cid = ? where uid = ?", [req.query.cid, req.user.uid], function(err, rows) {
+								if (err)
+									res.redirect('/confirm?t=e');
+								else
+									res.redirect('/confirm?t=rs');
+							})
+						}
+					})
+				}
+			})
+		}
+		else {
+			connection.query("SELECT bid from Users where uid = ?", [req.user.uid], function(err, rows) {
+				if (err)
+					res.redirect('/confirm?t=e');
+				else if (rows[0].bid == req.query.bid)
+					res.redirect('/confirm?t=ret');
+				else if (rows[0].bid != null)
+					res.redirect('/confirm?t=red');
+				else {
+					connection.query("UPDATE Bible SET current = (current + 1) where bid = ?", [req.query.bid], function(err1, rows1) {
+						if (err1)
+							res.redirect('/confirm?t=ref');
+						else {
+							connection.query("UPDATE Users SET bid = ? where uid = ?", [req.query.bid, req.user.uid], function(err, rows) {
+								if (err)
+									res.redirect('/confirm?t=e');
+								else
+									res.redirect('/confirm?t=rs');
+							})
+						}
+					})
+				}
+			})
+		}
 	})
 
 	app.get('/confirm', isLoggedIn, function(req, res){
 		res.render('confirm.ejs', {t : req.query.t});
 	});
 
-	app.get('/mycid', isLoggedIn, function(req, res) {
-		connection.query("SELECT * FROM Classes where cid = (SELECT cid FROM Users where uid = ?)", [req.user.uid], function(err, rows) {
-			if (!rows.length)
-				res.redirect('/confirm?t=me');
-			else
-				res.render('detail.ejs', {classinfo : rows[0], registered : true});
+	app.get('/my', isLoggedIn, function(req, res) {
+		connection.query("SELECT cid, bid FROM Users where uid = ?", [req.user.uid], function(err, rows) {
+			connection.query("SELECT * FROM Classes where cid = ?", [rows[0].cid], function(err1, rows1) {
+				connection.query("SELECT * FROM Bible where bid = ?", [rows[0].bid], function(err2, rows2) {
+					if (err || err1 || err2)
+						res.redirect('/confirm?t=e');
+					var creg = true;
+					var breg = true;
+					if (!rows1.length)
+						creg = false;
+					if (!rows2.length)
+						breg = false;
+					res.render('my.ejs', {creg : creg, breg : breg, classinfo : rows1[0], bibleinfo : rows2[0]});
+				})
+			})
 		})
 	});
-	
 };
 
 // route middleware to make sure
